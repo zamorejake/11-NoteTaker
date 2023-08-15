e9d6b975fcb76f601e6eeb8d312b0b76d45fddd7
Jake <zamorejake@gmail.com>
2023-08-15 15:58:18 -0500

    finished project and added DELETE route

--- routes/db.js
+++ routes/db.js
@@ -1,13 +1,102 @@
-const db = require('express').Router();
-const { v4: uuidv4 } = require('uuid');
-const fs = require('fs');
-const path = require('path');
+const db = require("express").Router();
+const { v4: uuidv4 } = require("uuid");
+const fs = require("fs");
+const path = require("path");
 
-db.get('/api/notes', (_req, res) =>
-  fs.readFile(path.join(__dirname, '../db/db.json'),'utf8', function(err, data){
+db.get("/api/notes", (_req, res) =>
+  fs.readFile(
+    path.join(__dirname, "../db/db.json"),
+    "utf8",
+    function (err, data) {
       let parsedData = JSON.parse(data);
-      err ? res.status(500).json("Error with retrieving notes") : res.status(200).json(parsedData);
-})
+      err
+        ? res.status(500).json("Error with retrieving notes")
+        : res.status(200).json(parsedData);
+    }
+  )
 );
+
+db.post("/api/notes", (req, res) => {
+  const { title, text } = req.body;
+  const isSuccess = title && text;
+
+  const newNote = {
+    title,
+    text,
+    id: uuidv4(),
+  };
+
+  const response = {
+    status: "success",
+    body: newNote,
+  };
+
+  fs.readFile(
+    path.join(__dirname, "../db/db.json"),
+    "utf8",
+    function (err, data) {
+      let parsedData = JSON.parse(data);
+      if (err) {
+        res.status(500).json("Error with reading note");
+      }
+      try {
+        parsedData.push(newNote);
+        const updateData = JSON.stringify(parsedData, null, 2);
+
+        fs.writeFile(
+          path.join(__dirname, "../db/db.json"),
+          updateData,
+          "utf8",
+          (err) => {
+            if (err) {
+              console.error(err);
+              return;
+            }
+          }
+        );
+      } catch (err) {
+        console.error(err);
+      }
+    }
+  );
+  isSuccess
+    ? res.json(response)
+    : res.status(500).json("Error in creating note");
+});
+
+db.delete("/api/notes/:id", (req, res) => {
+  const noteId = req.params.id;
+  fs.readFile(
+    path.join(__dirname, "../db/db.json"),
+    "utf8",
+    function (err, data) {
+      let parsedData = JSON.parse(data);
+      if (err) {
+        res.status(500).json("Error with reading note");
+      }
+      try {
+        const deleteData = parsedData.filter((json) => json.id !== noteId);
+        const updateData = JSON.stringify(deleteData, null, 2);
+
+        deleteData > updateData
+          ? res.json(`The note with the id:${noteId} was deleted!`)
+          : res.status(500).json("Error in deleting note");
+        fs.writeFile(
+          path.join(__dirname, "../db/db.json"),
+          updateData,
+          "utf8",
+          (err) => {
+            if (err) {
+              console.error(err);
+              return;
+            }
+          }
+        );
+      } catch (err) {
+        console.error(err);
+      }
+    }
+  );
+});
 
 module.exports = db;
--- server.js
+++ server.js
@@ -1,20 +1,19 @@
-const express = require('express');
-const db = require('./routes/db.js');
+const express = require("express");
+const db = require("./routes/db.js");
 const PORT = process.env.port || 3001;
-const path = require('path');
-
+const path = require("path");
 const app = express();
-app.use(db);
 app.use(express.json());
+app.use(db);
 app.use(express.urlencoded({ extended: true }));
-app.use(express.static('public'));
+app.use(express.static("public"));
 
-app.get('/notes', (_req, res) =>
-  res.sendFile(path.join(__dirname, '/public/notes.html'))
+app.get("/notes", (_req, res) =>
+  res.sendFile(path.join(__dirname, "/public/notes.html"))
 );
 
-app.get('*', (_req, res) =>
-  res.sendFile(path.join(__dirname, '/public/index.html'))
+app.get("*", (_req, res) =>
+  res.sendFile(path.join(__dirname, "/public/index.html"))
 );
 
 app.listen(PORT, () =>
